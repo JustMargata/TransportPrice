ee4d2031ad2efc009aa6974141b203074db80474
JustMargata <66246000+JustMargata@users.noreply.github.com>
2024-10-09 19:44:55 +0300

    Added Display.cs

---
+++ MVC/TransportPrice/View/Display.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportPrice.View
+{
+    internal class Display
+    {
+        public int Kilometers { get; set; }
+        public string DayOrNight { get; set; }
+        public double Price { get; set; }
+        public void Input()
+        {
+            this.Kilometers = int.Parse(Console.ReadLine());
+            this.DayOrNight = Console.ReadLine();
+        }
+        public void Output() 
+        {
+            Console.WriteLine(this.Price);
+        }
+    }
+}
